bf5e81ddd576ed22ffca92a2e191ac84cb11f671
KIRAN M R <iamthekiranmr@gmail.com>
2022-08-08 23:57:18 +0530

    checkbox working

--- src/App.js
+++ src/App.js
@@ -22,17 +22,26 @@ function App() {
           placeholder="🖊️ Add item..."
         />
         <i
-          onClick={() => setToDos([...toDos, toDo])}
+          onClick={() => setToDos([...toDos, {id:Date.now(),text:toDo,status:false}])}
           className="fas fa-plus"
         ></i>
       </div>
       <div className="todos">
-        {toDos.map((value) => {
+        {toDos.map((obj) => {
           return (
           <div className="todo">
             <div className="left">
-              <input type="checkbox" name="" id="" />
-              <p>{value}</p>
+              <input onChange={(e)=>{
+                console.log(e.target.checked)
+                console.log(obj)
+                setToDos(toDos.filter(obj2=>{
+                  if(obj2.id===obj.id){
+                    obj2.status=e.target.checked
+                  }
+                  return obj2
+                }))
+              }} value={obj.status} type="checkbox" name="" id="" />
+              <p>{obj.text}</p>
             </div>
             <div className="right">
               <i className="fas fa-times"></i>
